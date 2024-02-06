a88cbb1523f0a114a5be79f7704ff273e22a6dec
sanath waghela <86285868+Sanathw12@users.noreply.github.com>
2024-02-06 14:26:35 +0530

    Update ec2_instance_bash_script.sh

--- ec2_instance_bash_script.sh
+++ ec2_instance_bash_script.sh
@@ -6,12 +6,12 @@ instance_type_without_quotes=$(echo $instance_type | sed 's/"//g')
 echo $instance_type_without_quotes
 case $instance_type_without_quotes in
 "g4dn.xlarge")
-unzip -o /home/ubuntu/<zip_filename> -d /home/ubuntu/<folder_name> && unzip -o /home/ubuntu/<zip_filename> -d /home/ubuntu/<folder_name>
+unzip -o /home/ubuntu/<zip_filename> -d /home/ubuntu/<folder_name> && unzip -o /home/ubuntu/<zip_filename> -d /home/ubuntu/<folder_name>1
 systemctl start <service_name> && systemctl start <service_name>
 echo "Deployed on g4dn.xlarge."
 ;;
 "g4dn.2xlarge")
-unzip -o /home/ubuntu/<zip_filename>  -d /home/ubuntu/<folder_name> && unzip -o /home/ubuntu/<zip_filename> -d /home/ubuntu/<folder_name> && unzip -o /home/ubuntu/<zip_filename> -d /home/ubuntu/<folder_name>
+unzip -o /home/ubuntu/<zip_filename>  -d /home/ubuntu/<folder_name> && unzip -o /home/ubuntu/<zip_filename> -d /home/ubuntu/<folder_name>1 && unzip -o /home/ubuntu/<zip_filename> -d /home/ubuntu/<folder_name>2
 systemctl start <service_name> && systemctl start <service_name> && systemctl start <service_name>
 echo "Deployed on g4dn.2xlarge."
 ;;
