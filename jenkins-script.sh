a0de30594494c72cb3bcaf43564d246006a1af90
sanath waghela <86285868+Sanathw12@users.noreply.github.com>
2024-02-06 14:16:45 +0530

    Create jenkins-script.sh

---
+++ jenkins-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for i in `aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name asg-name | grep -i instanceid | awk '{ print $2}' | cut -d',' -f1| sed -e 's/"//g'`
+do
+instanceIp=`aws ec2 describe-instances --instance-ids $i | grep -i PrivateIpAddress | awk '{ print $2 }' | head -1 | cut -d"," -f1 | cut -d'"' -f2`
+echo "Instance found: " $instanceIp
+scp -i "<ssh_key_name>.pem" -o StrictHostKeyChecking=no ./<zip_filename>.zip ubuntu@$instanceIp:/home/ubuntu/
+ssh -i "<ssh_key_name>.pem" -o StrictHostKeyChecking=no ubuntu@$instanceIp 'sudo bash <bash_scrpt_on_ec2_instance>.sh'
+echo "Script Updated on this node!"
+done;
