7f350a1a45e55d79a114a139953fe5f856a8fb5d
sanath waghela <86285868+Sanathw12@users.noreply.github.com>
2024-02-16 13:36:27 +0530

    Update jenkins-script.sh

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -4,6 +4,6 @@ do
 instanceIp=`aws ec2 describe-instances --instance-ids $i | grep -i PrivateIpAddress | awk '{ print $2 }' | head -1 | cut -d"," -f1 | cut -d'"' -f2`
 echo "Instance found: " $instanceIp
 scp -i "<ssh_key_name>.pem" -o StrictHostKeyChecking=no ./<zip_filename>.zip ubuntu@$instanceIp:/home/ubuntu/
-ssh -i "<ssh_key_name>.pem" -o StrictHostKeyChecking=no ubuntu@$instanceIp 'sudo bash <bash_scrpt_on_ec2_instance>.sh'
+ssh -i "<ssh_key_name>.pem" -o StrictHostKeyChecking=no ubuntu@$instanceIp 'sudo bash ec2_instance_bash_script.sh'
 echo "Script Updated on this node!"
 done;
